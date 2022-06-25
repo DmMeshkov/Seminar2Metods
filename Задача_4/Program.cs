db6118ed8dd086e74018b34b5d4288241c67d390
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-25 21:34:55 +0500

    Задача программа проверяет делится ли число на 7 и 23 одновременно

---
+++ Задача_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*int Check(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Число делится");
+    }
+    else Console.WriteLine("Число не делится");
+    return 0;
+}
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int check = Check(number);
+Console.WriteLine(check);*/
+
+bool Check(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+        return true;
+    else
+        return false;
+}
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool res = Check(number);
+Console.WriteLine(res);

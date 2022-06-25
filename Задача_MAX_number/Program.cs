3d213d13f10664929d30c8c609f84a2231c69140
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-25 14:51:33 +0500

    задача генерирует число от 10 до 99 и показывает наибольшую цифру этого числа

---
+++ Задача_MAX_number/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа генерирует случайное число из отрезка от 10 до 99 и показывает наибольшую цифру сгенерированного числа.
+
+void ShowBiggerDigit()
+{
+    int rand = new Random().Next(10, 100);//100 не учитывается. будет 99
+    Console.WriteLine("Our current number is " + rand);
+    int dec = rand / 10;
+    int ed = rand % 10;
+    int max;
+    if (dec > ed) max = dec;
+    else max = ed;
+    Console.WriteLine("Bigger digit is " + max);
+
+}
+
+ShowBiggerDigit();
